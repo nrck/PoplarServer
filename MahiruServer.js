d17cfca3a682fd9f91aeb592535838989f5019f5
nrck <challengerk.8000@gmail.com>
2017-12-11 00:16:32 +0900

    mahiru update

--- MahiruServer.js
+++ MahiruServer.js
@@ -5,10 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-//var index = require('./routes/index');
-//var users = require('./routes/users');
-var samples = require('./api/sample');
-var job = require('./api/1/job');
+var apiDir = '/api/1/';
+var job = require('.' + apiDir + 'job');
+var jobnet = require('.' + apiDir + 'jobnet');
+var history = require('.' + apiDir + 'history');
+var instance = require('.' + apiDir + 'instance');
 
 var app = express();
 
@@ -24,20 +25,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', index);
-//app.use('/users', users);
-app.use('/api/samples', samples); 
-app.use('/api/1/job', job); 
+app.use(apiDir + 'job', job);
+app.use(apiDir + 'jobnet', jobnet);
+//app.use(apiDir + 'history', history);
+//app.use(apiDir + 'instance', instance);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
