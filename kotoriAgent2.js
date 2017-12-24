ded3abc666f2e605191010ecc0cdc698fea2e1f3
nrck <challengerk.8000@gmail.com>
2017-12-25 00:14:45 +0900

    ひとまずスタートラインに立てた

--- kotoriAgent2.js
+++ kotoriAgent2.js
@@ -3,7 +3,7 @@
  */
 
 // 設定
-var config = {
+const config = {
     "server": {
         "ip": "192.168.2.2",
         "port": "27133",
@@ -13,9 +13,10 @@ var config = {
     }
 }
 
-var io = require('socket.io-client');
-var interval = 1;
-var socket = null;
+const io = require('socket.io-client');
+const child_process = require('child_process');
+let interval = 1;
+let socket = null;
 
 function open() {
     socket = io('ws://' + config.server.ip + ':' + config.server.port, {
@@ -28,32 +29,44 @@ function open() {
         socket.json.emit("whoami", JSON.parse('{"agentName":"' + config.agent.name + '"}'));
         socket.on("result", result);
         socket.on('disconnect', reConnect);
+        socket.on('job', receiveJob);
     });
-
     socket.connect();
 }
 open();
 
-setTimeout(function () {
-    // 接続を切る
-    socket.disconnect();
+const receiveJob = function (data) {
+    //sendJson = {
+    //    id: id,
+    //    jobcode: jobcode,
+    //    program: job.program,
+    //    command: job.command
+    //}
+    console.info(data)
+    let json = JSON.parse(JSON.stringify(data));
+    let stdio = null;
+    switch (json.program) {
+        case "shell":
+            break;
+        case "powershell":
+            stdio = child_process.execSync(json.command);
+            break;
+    };
+    json.stdio = stdio.toString();
+    socket.json.emit("jobresult", json);
+}
 
-    // 再接続
-    //socket.connect();  // -> connected
-}, 3000);
-
-
-var result = function (data) {
+const result = function (data) {
     console.log("result : " + data.result);
     console.log(data.data);
 }
 
-var reConnect = function (reason) {
+const reConnect = function (reason) {
     console.log("切断されました。（" + reason + "）");
     socket.disconnect(true);
     //socket = null;
     // 待ち時間＝再接続時間^2 * 100ms（最大60秒）
-    var waitTime = ((interval * interval * 100) >= 60000 * 5) ? 60000 * 5 : interval * interval * 100;
+    let waitTime = ((interval * interval * 100) >= 60000 * 5) ? 60000 * 5 : interval * interval * 100;
     console.log("再接続" + interval + "回目（" + waitTime + "ミリ秒後に再接続をします）");
     setTimeout(function () {
         open();
--- poplarServer2.js
+++ poplarServer2.js
@@ -6,10 +6,9 @@
 const agentJSON = require('./agent.config.json')
 const ioAgent = require("socket.io")()
 const port = { kotori: "27133", mahiru: "27132" }
-const async = require('async');
 
 const __queuingJson = "./que.json";
-const __doingJson = "./do.json";
+const __runningJson = "./run.json";
 const __historyJson = "./history.json";
 const __fileUtil = require('./common.js').fileUtil();
 
@@ -32,7 +31,19 @@ ioAgent.sockets.on("connection", function (socket) {
         }
         console.info("[" + tmp.result + "] " + data.agentName)
         console.info("[Total instance] " + Object.keys(instance).length)
-        socket.json.emit("result", json)
+        socket.json.emit("result", tmp)
+    });
+
+    socket.on("jobresult", function (data) {
+        let tmp = {};
+        if (true) {
+            tmp = { result: "success", data: data }
+        } else {
+            tmp = { result: "false", data: data }
+        }
+        console.info("[Job exec result] id => " + data.id + " jobcode => " + data.jobcode + " stdio =>" + data.stdio)
+        socket.json.emit("result", tmp)
+        finishJob(data.id, data.jobcode);
     });
 
     // インスタンスとの接続が切れた時
@@ -56,15 +67,142 @@ setQueJobnetAll();
  * @param {string} agentName 発行対象のエージェント 
  */
 function sendJob(agentName, job) {
-    // エージェントは生きているか？
-    // jsondbに登録
-    // すでに実行済みではないか検証
-    // 
     instance[agentName].socket.json.emit("job", job);
 }
 
-function controlJobnet(id) {
 
+function controlJobnet(id) {
+    // 全実行中ジョブネットを読み込み
+    let runs = JSON.parse(__fileUtil.read(__runningJson));
+    let run = runs[id];
+    //console.info(run)
+
+    // コントロールスタート
+    if (run.jobnet.jobs.end.state == "finish") {
+        deleteRunningJsonById(id)
+
+        run.header.state = "finish";
+        console.info("[Finish jobnet] id => " + id)
+    }
+    if (run.header.state == "waiting") {
+        run.header.state = "running";
+
+        // スタートに設定されているジョブを全て実行する。
+        for (let i = 1; i < run.jobnet.nextMatrix[0].length; i++) {
+            if (0 < run.jobnet.nextMatrix[0][i]) {
+                run.jobnet.nextMatrix[i][0] = 1;
+                controlJob(id, run.jobnet.jobArray[i])
+            }
+        }
+    }
+
+    // ジョブネット遅延監視
+
+    // ジョブネット打ち切り
+
+    // ステータス書き換え後のジョブを書き出し
+    runs[id] = run;
+    if (__fileUtil.write(__runningJson, JSON.stringify(runs, null, "    "))) {
+        console.info("[Updata running] id => " + id)
+    } else {
+        console.info("[Updata running error] id => " + id)
+    }
+}
+
+function controlJob(id, jobcode) {
+    // 全実行中ジョブネットを読み込み
+    let runs = JSON.parse(__fileUtil.read(__runningJson));
+    let job = runs[id].jobnet.jobs[jobcode];
+
+    // コントロールスタート
+    console.info("[Control Job] id => " + id + " jobcode => " + jobcode)
+    switch (job.state) {
+        case "waiting":
+            // 前ジョブが終了済みか確認する。
+            for (let i = 0; i < runs[id].jobnet.jobArray.length; i = i + 1) {
+                let flag = true;
+                //console.info(runs[id].jobnet.jobArray[i] + " == " + jobcode)
+                if (runs[id].jobnet.jobArray[i] == jobcode) {
+                    for (let j = 0; j < runs[id].jobnet.nextMatrix.length; j = j + 1) {
+                        //console.info("(" + j + ", " + i + ") = " + runs[id].jobnet.nextMatrix[j][i] + " (" + i + ", " + j + ") = " + runs[id].jobnet.nextMatrix[i][j])
+                        if (0 < runs[id].jobnet.nextMatrix[j][i] && 0 == runs[id].jobnet.nextMatrix[i][j]) {
+                            flag = false;
+                            break;
+                        }
+                    }
+
+                    //console.info(flag)
+                    if (flag) {
+                        if (jobcode == "end") {
+                            job.state = "finish";
+                            setTimeout(controlJobnet, 1000, id);
+                        } else {
+                            job.state = "running";
+                            console.info("[Start job] id => " + id + ", jobcode => " + jobcode)
+                            let sendJson = {
+                                id: id,
+                                jobcode: jobcode,
+                                program: job.program,
+                                command: job.command
+                            }
+                            sendJob(job.agentName, sendJson);
+                        }
+                    } else {
+                        setTimeout(controlJob, 1000, id, jobcode)
+                    }
+                }
+            }
+            break;
+        case "running":
+            break;
+        case "error":
+            // 次のエラー時ジョブを検索して実行開始する。
+            for (let i = 0; i < runs[id].jobnet.jobArray.length; i = i + 1) {
+                if (runs[id].jobnet.jobArray[i] == jobcode) {
+                    for (let j = 0; j < runs[id].jobnet.errorMatrix[i].length; j = j + 1) {
+                        if (0 < runs[id].jobnet.errorMatrix[i][j]) {
+                            runs[id].jobnet.errorMatrix[j][i] = 1;
+                            controlJob(id, run.jobnet.jobArray[j])
+                        }
+                    }
+                }
+            }
+        case "finish":
+            // 次のジョブを検索して実行開始する。
+            console.info("[Finish job] id => " + id + ", jobcode => " + jobcode)
+            for (let i = 0; i < runs[id].jobnet.jobArray.length; i = i + 1) {
+                if (runs[id].jobnet.jobArray[i] == jobcode) {
+                    for (let j = 0; j < runs[id].jobnet.nextMatrix[i].length; j = j + 1) {
+                        if (0 < runs[id].jobnet.nextMatrix[i][j] && 0 == runs[id].jobnet.nextMatrix[j][i]) {
+                            runs[id].jobnet.nextMatrix[j][i] = 1;
+                            controlJob(id, runs[id].jobnet.jobArray[j])
+                        }
+                    }
+                }
+            }
+            break;
+    }
+    // ステータス書き換え後のジョブを書き出し
+    runs[id].jobnet.jobs[jobcode] = job;
+    if (__fileUtil.write(__runningJson, JSON.stringify(runs, null, "    "))) {
+        console.info("[Updata running] id => " + id + ", jobcode => " + jobcode)
+    } else {
+        console.info("[Updata running error] id => " + id + ", jobcode => " + jobcode)
+    }
+}
+
+function finishJob(id, jobcode) {
+    // 全実行中ジョブネットを読み込み
+    let runs = JSON.parse(__fileUtil.read(__runningJson));
+    runs[id].jobnet.jobs[jobcode].state = "finish";
+
+    // ステータス書き換え後のジョブを書き出し
+    if (__fileUtil.write(__runningJson, JSON.stringify(runs, null, "    "))) {
+        console.info("[Updata running] id => " + id + ", jobcode => " + jobcode + " Finish")
+    } else {
+        console.info("[Updata running error] id => " + id + ", jobcode => " + jobcode + " Finish")
+    }
+    controlJob(id, jobcode);
 }
 
 /**
@@ -74,13 +212,15 @@ function controlJobnet(id) {
  * @return {boolean} 真偽値を返す
  */
 function isPermitAgent(socket, agentName) {
+    let flag = false;
     agentJSON.forEach(obj => {
         if (obj.agentName == agentName) {
             instance[agentName] = { id: socket.id, socket: socket };
-            return true;
+            flag = true;
+            return
         }
     });
-    return false;
+    return flag
 }
 
 /**
@@ -89,6 +229,7 @@ function isPermitAgent(socket, agentName) {
 function readJobnet() {
     jobnets = null;
     jobnets = require('./jobnet.json')
+    setTimeout(readJobnet, 1000);
 }
 
 /**
@@ -120,12 +261,12 @@ function setQueJobnetAll() {
             // 同時刻の実行が登録されていないか確認（jobnetネームと開始時刻が重複してないか確認）
             if (!isQueuingJsonByName(jobnet.agentName, jobnetStartTime)) {
                 // 登録無しなら登録を実行
-                let id = putQueuingJson(jobnet, jobnetStartTime);
+                let id = appendQueuingJson(jobnet, jobnetStartTime);
                 if (id != null) {
-                    console.info("[Put Queuing] JobnetName => " + jobnet.jobnetName + ", Start at " + jobnetStartTime.getTime() + " (" + id + ")")
+                    console.info("[Append Queuing] JobnetName => " + jobnet.jobnetName + ", Start at " + jobnetStartTime.getTime() + " (" + id + ")")
                     setTimeout(queuingJobnet, waitTime, id);
                 } else {
-                    console.error("[Put Queuing Error] JobnetName => " + jobnet.jobnetName + ", Start at " + jobnetStartTime.getTime() + " (" + id + ")")
+                    console.error("[Append Queuing Error] JobnetName => " + jobnet.jobnetName + ", Start at " + jobnetStartTime.getTime() + " (" + id + ")")
                 }
             }
         }
@@ -148,10 +289,12 @@ function queuingJobnet(id) {
         console.info("Waiting... " + waitTime + "ms")
         setTimeout(queuingJobnet, 100 < waitTime ? waitTime / 2 : waitTime, id);
     } else {
-        // do.json
-        console.info("にゃーん 誤差" + waitTime + "ms")
-
-        controlJobnet(id);
+        console.info("Starting 誤差" + waitTime + "ms")
+        if (deleteQuingJsonById(id)) {
+            controlJobnet(appendRunningJson(que));
+        } else {
+            console.error("[Delete Queuing Error] Queuing ID => " + id + ", jobnet name => " + que.jobnet.jobnetName + "")
+        }
     }
 }
 
@@ -160,7 +303,7 @@ function queuingJobnet(id) {
  * @param {Object} jobnet 
  * @param {Date} jobnetStartTime 
  */
-function putQueuingJson(jobnet, jobnetStartTime) {
+function appendQueuingJson(jobnet, jobnetStartTime) {
     let id = jobnetId.getId();
     let que = new Object();
 
@@ -193,16 +336,6 @@ function putQueuingJson(jobnet, jobnetStartTime) {
     }
 }
 
-
-/**
- * キューイング一覧に情報を追加
- * @param {*} jobnet 
- * @param {*} jobnetStartTime 
- * @param {*} id 
- */
-function updatQueuingJson(jobnet, jobnetStartTime, id) {
-}
-
 /**
  * ジョブネットがキューイング一覧にあるかどうか
  * @param {String} jobnetName 
@@ -229,9 +362,9 @@ function selectQueuingJsonByName(jobnetName, jobnetStartTime) {
     if (queues == null) {
         return null
     }
-    queues.forEach(obj => {
-        if (obj.jobnet.agentName == jobnetName && obj.header.startTime == jobnetStartTime.getTime()) {
-            queue = obj;
+    queues.forEach(que => {
+        if (que.jobnet.agentName == jobnetName && que.header.startTime == jobnetStartTime.getTime()) {
+            queue = que;
             return
         }
     });
@@ -263,7 +396,19 @@ function selectQueuingJsonById(id) {
  * @param {*} jobnetStartTime 
  */
 function deleteQuingJsonByName(jobnetName, jobnetStartTime) {
+    let queues = JSON.parse(__fileUtil.read(__queuingJson));
+    let flag = false;
 
+    for (let i = 0; i < queues.length; i = i + 1) {
+        que = queues[i];
+        if (que.jobnet.jobnetName == jobnetName && que.header.startTime == jobnetStartTime.getTime()) {
+            queues.splice(i, 1);
+            flag = true;
+            return
+        }
+    }
+
+    return flag
 }
 
 /**
@@ -271,45 +416,107 @@ function deleteQuingJsonByName(jobnetName, jobnetStartTime) {
  * @param {*} id 
  */
 function deleteQuingJsonById(id) {
-    let que = null;
-    async.series([
-        function (callback) {
-            let queues = JSON.parse(__fileUtil.read(__queuingJson));
-            for (let i = 0; i < queues.length; i++) {
-                que = queues[i];
-                if (que.header.id == id) {
-                    queues.splice(i, 1);
-                    return
-                }
-            }
-            callback(null, null);
-        },
-        function (callback) {
-            let doing = JSON.parse(__fileUtil.read(__doingJson));
-            if (doing == null) {
-                doing = {};
-            }
-            doing[id] = que.jobnet;
-            if (__fileUtil.write(__queuingJson, JSON.stringify(doing, null, "    "))) {
-                return callback(null, null);
-            } else {
-                return callback("error", null);
-            }
-        },
-        function (callback) {
-            fs.readFile("file-C", "utf-8", function (err, data) {
-                console.log("file-C");
-                callback(null, "third")
-            });
+    let queues = JSON.parse(__fileUtil.read(__queuingJson));
+    let flag = false;
+
+    for (let i = 0; i < queues.length; i = i + 1) {
+        let que = queues[i];
+        if (que.header.id == id) {
+            queues.splice(i, 1);
+            flag = true;
+            break;
         }
-    ], function (err, results) {
-        if (err) {
-            throw err;
-        }
-        console.log('series all done. ' + results);
-    });
+    }
+    return flag;
 }
 
+function appendRunningJson(que) {
+    let run = que;
+    //console.info(run)
+    // 実行中ジョブネットファイルの存在確認
+    if (!__fileUtil.isExist(__runningJson)) {
+        // ファイルが無ければ作成
+        if (__fileUtil.write(__runningJson, "{}")) {
+            console.info("[File Create] " + __runningJson + " is created.")
+        } else {
+            return false
+        }
+    }
+
+    // 全実行中ジョブネットを読み込み
+    let json = JSON.parse(__fileUtil.read(__runningJson));
+    if (json == null) {
+        json = "{}";
+    }
+
+    // 実行中オブジェクトを作成
+    let date = new Date();
+    run.header.startTime = date.getTime();
+    run.header.state = "waiting";
+
+    // 全ジョブを待機中に変更
+    for (let i = 1; i < run.jobnet.jobArray.length; i = i + 1) {
+        let jobcode = run.jobnet.jobArray[i];
+        run.jobnet.jobs[jobcode].state = "waiting";
+    }
+
+    // 多分ジョブの開始時刻とか全部ここで設定した方がいい
+
+    // 全実行中ジョブネットに追加し書き込み
+    json[run.header.id] = run;
+    if (__fileUtil.write(__runningJson, JSON.stringify(json, null, "    "))) {
+        return run.header.id
+    } else {
+        return null
+    }
+}
+
+function deleteRunningJsonById(id) {
+    let runs = JSON.parse(__fileUtil.read(__runningJson));
+    let run = runs.id
+    delete runs.id
+    return true
+}
+
+function appendHistoryJson(run) {
+    let history = run;
+
+    // 全履歴ファイルの存在確認
+    if (!__fileUtil.isExist(__historyJson)) {
+        // ファイルが無ければ作成
+        if (__fileUtil.write(__historyJson, "[]")) {
+            console.info("[File Create] " + __historyJson + " is created.")
+        } else {
+            return false
+        }
+    }
+
+    // 全履歴を読み込み
+    let json = JSON.parse(__fileUtil.read(__historyJson));
+    if (json == null) {
+        json = "[]";
+    }
+
+    // 履歴オブジェクトを作成
+    let date = new Date();
+    history.header.finishTime = date.getTime();
+
+    // 全履歴に追加し書き込み
+    json.push(history);
+
+    // 終了時刻降順
+    json.sort(function (a, b) {
+        if (a.header.finishTime > b.header.finishTime) return -1;
+        if (a.header.finishTime < b.header.finishTime) return 1;
+        return 0;
+    });
+
+    if (__fileUtil.write(__historyJson, JSON.stringify(json, null, "    "))) {
+        return true
+    } else {
+        return false
+    }
+}
 
 
 /**
