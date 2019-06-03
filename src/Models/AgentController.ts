cd1dadef016e1a10f5860274fda4973ee6f78c55
nrck <challengerk.8000@gmail.com>
2019-06-03 22:49:31 +0900

    refact: agent controller

--- src/Models/AgentController.ts
+++ src/Models/AgentController.ts
@@ -22,9 +22,11 @@ export interface IAgentListResponse extends IAgentList, IResponseInfo { }
 export interface IAgentResponse extends IAgent, IResponseInfo { }
 
 declare type TAgentListResolve = (result?: IAgentListResponse | Promise<IAgentListResponse> | undefined) => void;
+
 declare type TAgentListReject = (reason?: IAgentListResponse | Promise<IAgentListResponse> | undefined) => void;
 
 declare type TAgentResolve = (result?: IAgentResponse | Promise<IAgentResponse> | undefined) => void;
+
 declare type TAgentReject = (reason?: IAgentResponse | Promise<IAgentResponse> | undefined) => void;
 
 export interface IPagingParameters {
@@ -36,9 +38,29 @@ export interface IPagingParameters {
  * Agent Controller
  */
 export class AgentController {
+
+    private static getAgentListResponse(agents: Agent[], message: string = '成功しました。', state: number = 200): IAgentListResponse {
+        return {
+            'agents': agents,
+            'message': message,
+            'state': state,
+            'timestamp': new Date(),
+            'total': agents.length
+        };
+    }
+
+    private static getAgentResponse(agent: Agent | undefined, message: string = '成功しました。', state: number = 200): IAgentResponse {
+        return {
+            'agent': agent,
+            'message': message,
+            'state': state,
+            'timestamp': new Date(),
+            'total': agent === undefined ? 0 : 1
+        };
+    }
+
     /** Return ALL AGENT list */
     public static async all(query: IPagingParameters): Promise<IAgentListResponse> {
-
         return new Promise(async (resolve: TAgentListResolve, reject: TAgentListReject): Promise<void> => {
             const opt: FindManyOptions<Agent> = {
                 'skip': query.offset || 0,
@@ -50,35 +72,12 @@ export class AgentController {
                 const agents = await conn.manager.find(Agent, opt);
 
                 if (agents.length !== 0) {
-                    const res: IAgentListResponse = {
-                        'agents': agents,
-                        'message': '読み込みに成功しました。',
-                        'state': 200,
-                        'timestamp': new Date(),
-                        'total': agents.length
-                    };
-
-                    resolve(res);
+                    resolve(AgentController.getAgentListResponse(agents, '読込に成功しました。'));
                 } else {
-                    const res: IAgentListResponse = {
-                        'agents': agents,
-                        'message': 'エージェントがありませんでした。',
-                        'state': 404,
-                        'timestamp': new Date(),
-                        'total': agents.length
-                    };
-
-                    reject(res);
+                    reject(AgentController.getAgentListResponse(agents, 'エージェントがありませんでした。', 404));
                 }
             } catch (err) {
-                const reason: IAgentListResponse = {
-                    'agents': [],
-                    'message': err.message,
-                    'state': 500,
-                    'timestamp': new Date(),
-                    'total': 0
-                };
-                reject(reason);
+                reject(AgentController.getAgentListResponse([], err.message, 500));
             }
         });
     }
@@ -88,40 +87,15 @@ export class AgentController {
         return new Promise(async (resolve: TAgentResolve, reject: TAgentReject): Promise<void> => {
             try {
                 const conn = await DataStore.createConnection();
-                // ID指定で1件だけ取得
                 const result = await conn.manager.findOne<Agent>(Agent, id);
 
                 if (typeof result !== 'undefined') {
-                    const res: IAgentResponse = {
-                        'agent': result,
-                        'message': '読み込みに成功しました。',
-                        'state': 200,
-                        'timestamp': new Date(),
-                        'total': 1
-                    };
-
-                    resolve(res);
+                    resolve(AgentController.getAgentResponse(result, '読込に成功しました。'));
                 } else {
-                    const res: IAgentResponse = {
-                        'agent': undefined,
-                        'message': 'エージェントがありませんでした。',
-                        'state': 404,
-                        'timestamp': new Date(),
-                        'total': 0
-                    };
-
-                    reject(res);
+                    reject(AgentController.getAgentResponse(result, 'エージェントがありませんでした。', 404));
                 }
             } catch (err) {
-                const reason: IAgentResponse = {
-                    'agent': undefined,
-                    'message': err.message,
-                    'state': 500,
-                    'timestamp': new Date(),
-                    'total': 0
-                };
-
-                reject(reason);
+                reject(AgentController.getAgentResponse(undefined, err.message, 500));
             }
         });
     }
@@ -129,42 +103,18 @@ export class AgentController {
     /** Insert an Agent */
     public static async add(param: IAgent): Promise<IAgentResponse> {
         return new Promise(async (resolve: TAgentResolve, reject: TAgentReject): Promise<void> => {
-            if (typeof param.agent === 'undefined') {
-                reject({
-                    'agent': undefined,
-                    'message': 'エージェントを指定してください。',
-                    'state': 400,
-                    'timestamp': new Date(),
-                    'total': 0
-                });
+            if (param.agent === undefined) {
+                reject(AgentController.getAgentResponse(undefined, 'エージェントを指定してください。', 400));
 
                 return;
             }
-            const agent = param.agent;
 
             try {
                 const conn = await DataStore.createConnection();
-                await conn.manager.insert(Agent, agent);
-
-                const res: IAgentResponse = {
-                    'agent': agent,
-                    'message': '書き込みに成功しました。',
-                    'state': 200,
-                    'timestamp': new Date(),
-                    'total': 1
-                };
-
-                resolve(res);
+                await conn.manager.insert(Agent, param.agent);
+                resolve(AgentController.getAgentResponse(param.agent, '書き込みに成功しました。'));
             } catch (err) {
-                const reason: IAgentResponse = {
-                    'agent': agent,
-                    'message': err.message,
-                    'state': 500,
-                    'timestamp': new Date(),
-                    'total': 0
-                };
-
-                reject(reason);
+                reject(AgentController.getAgentResponse(param.agent, err.message, 500));
             }
         });
     }
@@ -172,7 +122,11 @@ export class AgentController {
     /** Update an agent */
     public static async update(param: IAgent): Promise<IAgentResponse> {
         return new Promise(async (resolve: TAgentResolve, reject: TAgentReject): Promise<void> => {
-            if (param.agent === undefined) return;
+            if (param.agent === undefined) {
+                reject(AgentController.getAgentResponse(undefined, 'エージェントを指定してください。', 400));
+
+                return;
+            }
 
             const opt: FindConditions<Agent> = {
                 'ipaddress': param.agent.ipaddress,
@@ -184,13 +138,7 @@ export class AgentController {
                 const res = await conn.getRepository(Agent).findOne(opt);
 
                 if (res === undefined) {
-                    reject({
-                        'agent': param.agent,
-                        'message': '指定されたエージェントは存在しませんでした。',
-                        'state': 404,
-                        'timestamp': new Date(),
-                        'total': 0
-                    });
+                    reject(AgentController.getAgentResponse(param.agent, '指定されたエージェントは存在しませんでした。', 404));
 
                     return;
                 }
