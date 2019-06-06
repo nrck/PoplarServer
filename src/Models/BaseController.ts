d41927aaa1192467889bedb7350cc1ef09a56503
nrck <challengerk.8000@gmail.com>
2019-06-06 10:28:08 +0900

    change: refact base controller

--- src/Models/BaseController.ts
+++ src/Models/BaseController.ts
@@ -2,6 +2,7 @@ import { BaseEntity, FindConditions, FindManyOptions, ObjectType } from 'typeorm
 import { DataStore } from './DataStore';
 import { IPaging } from './Interface/Parameter';
 import { IBaseResponse } from './Interface/Response';
+import { BAD_REQUEST, NOT_FOUND, SERVER_ERROR, SUCCESS } from './Types/HttpStateCode';
 
 export interface IResponse<T> extends IBaseResponse {
     /** Response entity. object, array or undefined. */
@@ -22,7 +23,8 @@ export class BaseController {
      * @param message result message
      * @param state This is HTTP State code. 200 is OK. 500 is error.
      */
-    private static getResponse<T>(entity: T | T[] | undefined, message = 'Success.', state = 200): IResponse<T> {
+    // tslint:disable-next-line: no-inferrable-types
+    private static getResponse<T>(entity: T | T[] | undefined, message: string = 'Success.', state: number = SUCCESS): IResponse<T> {
         let total = 0;
         if (Array.isArray(entity)) {
             total = entity.length;
@@ -41,10 +43,12 @@ export class BaseController {
 
     /** Return ALL MASTER JOB list */
     public static async all<T extends BaseEntity>(entityClass: ObjectType<T>, query: IPaging): Promise<IResponse<T>> {
-        return new Promise(async(resolve: FuncResolve<T>, reject: FuncReject<T>): Promise<void> => {
+        // tslint:disable-next-line: space-before-function-paren
+        return new Promise(async (resolve: FuncResolve<T>, reject: FuncReject<T>): Promise<void> => {
             const opt: FindManyOptions<T> = {
-                'skip': query.offset || 0,
-                'take': query.limit || 100
+                'skip': query.offset === undefined ? 0 : query.offset,
+                // tslint:disable-next-line: no-magic-numbers
+                'take': query.limit === undefined ? 100 : query.limit
             };
 
             try {
@@ -52,39 +56,41 @@ export class BaseController {
                 const objects = await conn.manager.find<T>(entityClass, opt);
 
                 if (objects.length === 0) {
-                    reject(BaseController.getResponse<T>(objects, `${entityClass.name} is not found.`, 404));
+                    reject(BaseController.getResponse<T>(objects, `${entityClass.name} is not found.`, NOT_FOUND));
                 } else {
                     resolve(BaseController.getResponse<T>(objects));
                 }
             } catch (err) {
-                reject(BaseController.getResponse<T>(undefined, err.message, 500));
+                reject(BaseController.getResponse<T>(undefined, (err as Error).message, SERVER_ERROR));
             }
         });
     }
 
     /** Get an master job by id */
     public static async get<T extends BaseEntity>(entityClass: ObjectType<T>, id: number): Promise<IResponse<T>> {
-        return new Promise(async(resolve: FuncResolve<T>, reject: FuncReject<T>): Promise<void> => {
+        // tslint:disable-next-line: space-before-function-paren
+        return new Promise(async (resolve: FuncResolve<T>, reject: FuncReject<T>): Promise<void> => {
             try {
                 const conn = await DataStore.createConnection();
                 const object = await conn.manager.findOne<T>(entityClass, id);
 
                 if (object === undefined) {
-                    reject(BaseController.getResponse<T>(object, `${entityClass.name} is not found.`, 404));
+                    reject(BaseController.getResponse<T>(object, `${entityClass.name} is not found.`, NOT_FOUND));
                 } else {
                     resolve(BaseController.getResponse<T>(object));
                 }
             } catch (err) {
-                reject(BaseController.getResponse<T>(undefined, err.message, 500));
+                reject(BaseController.getResponse<T>(undefined, (err as Error).message, SERVER_ERROR));
             }
         });
     }
 
     /** Insert an Agent */
     public static async add<T>(entityClass: ObjectType<T>, param: T): Promise<IResponse<T>> {
-        return new Promise(async(resolve: FuncResolve<T>, reject: FuncReject<T>): Promise<void> => {
+        // tslint:disable-next-line: space-before-function-paren
+        return new Promise(async (resolve: FuncResolve<T>, reject: FuncReject<T>): Promise<void> => {
             if (param === undefined) {
-                reject(BaseController.getResponse<T>(undefined, `Your ${entityClass.name} parameter is bad request.`, 400));
+                reject(BaseController.getResponse<T>(undefined, `Your ${entityClass.name} parameter is bad request.`, BAD_REQUEST));
 
                 return;
             }
@@ -94,20 +100,22 @@ export class BaseController {
                 await conn.manager.insert<T>(entityClass, param);
                 resolve(BaseController.getResponse<T>(param));
             } catch (err) {
-                reject(BaseController.getResponse<T>(param, err.message, 500));
+                reject(BaseController.getResponse<T>(param, (err as Error).message, SERVER_ERROR));
             }
         });
     }
 
     /** Update an agent */
+    // tslint:disable-next-line: completed-docs
     public static async update<T extends { id: number }>(entityClass: ObjectType<T>, entity: T, opt: FindConditions<T>): Promise<IResponse<T>> {
-        return new Promise(async(resolve: FuncResolve<T>, reject: FuncReject<T>): Promise<void> => {
+        // tslint:disable-next-line: space-before-function-paren
+        return new Promise(async (resolve: FuncResolve<T>, reject: FuncReject<T>): Promise<void> => {
             try {
                 const conn = await DataStore.createConnection();
                 const findone = await conn.getRepository(entityClass).findOne(opt);
 
                 if (findone === undefined) {
-                    reject(BaseController.getResponse<T>(undefined, `${entityClass.name} is not found.`, 404));
+                    reject(BaseController.getResponse<T>(undefined, `${entityClass.name} is not found.`, NOT_FOUND));
 
                     return;
                 }
@@ -117,21 +125,22 @@ export class BaseController {
                 resolve(BaseController.getResponse<T>(object));
 
             } catch (err) {
-                reject(BaseController.getResponse<T>(entity, err.message, 500));
+                reject(BaseController.getResponse<T>(entity, (err as Error).message, SERVER_ERROR));
             }
         });
     }
 
     /** Delete an agent by ID */
     public static async delete<T extends BaseEntity>(entityClass: ObjectType<T>, id: number): Promise<IResponse<T>> {
-        return new Promise(async(resolve: FuncResolve<T>, reject: FuncReject<T>): Promise<void> => {
+        // tslint:disable-next-line: space-before-function-paren
+        return new Promise(async (resolve: FuncResolve<T>, reject: FuncReject<T>): Promise<void> => {
             try {
                 const conn = await DataStore.createConnection();
                 const rep = conn.getRepository(entityClass);
                 const object = await rep.findOne(id);
 
                 if (object === undefined) {
-                    reject(BaseController.getResponse<T>(object, `${entityClass.name} is not found.`, 404));
+                    reject(BaseController.getResponse<T>(object, `${entityClass.name} is not found.`, NOT_FOUND));
 
                     return;
                 }
@@ -139,7 +148,7 @@ export class BaseController {
                 await rep.remove(object);
                 resolve(BaseController.getResponse<T>(object));
             } catch (err) {
-                reject(BaseController.getResponse<T>(undefined, err.message, 500));
+                reject(BaseController.getResponse<T>(undefined, (err as Error).message, SERVER_ERROR));
             }
         });
     }
