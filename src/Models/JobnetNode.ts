eb9c5d008494a40d027558880729ec5aeebd30f2
nrck <challengerk.8000@gmail.com>
2019-06-10 11:24:10 +0900

    doc: JobnetNode.ts

--- src/Models/JobnetNode.ts
+++ src/Models/JobnetNode.ts
@@ -1,19 +1,28 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
 
+/**
+ * Jobnet Node
+ */
 @Entity()
+@Unique('UQ_JOBNET_NODE', ['jobnetId', 'sourceJobId', 'targetSuccessJobId'])
 export class JobnetNode extends BaseEntity {
+    /** This column is Primary key */
     @PrimaryGeneratedColumn()
     public readonly id!: number;
 
-    @Column()
-    public jobnetId: number;
+    /** ID of the jobnet that contains this node. */
+    @Column('int', { 'nullable': false })
+    public jobnetId!: number;
 
-    @Column()
-    public sorceJobId: number;
+    /** ID is the sourece job of this node. */
+    @Column('int', { 'nullable': false })
+    public sourceJobId!: number;
 
-    @Column()
-    public targetSuccessJobId: number;
+    /** ID is the target job(at SUCCESS) of this node. */
+    @Column('int', { 'nullable': false })
+    public targetSuccessJobId!: number;
 
-    @Column()
-    public targetErrorJobId: number;
+    /** ID is the target job(at ERROR) of this node. */
+    @Column('int', { 'nullable': false })
+    public targetErrorJobId!: number;
 }
