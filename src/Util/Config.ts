175a3d2b70935ba681eabfc6e6bb80c53b0e89ca
nrck <challengerk.8000@gmail.com>
2019-06-17 17:24:57 +0900

    change: move Config.ts

---
+++ src/Util/Config.ts
@@ -0,0 +1,34 @@
+import { readFileSync } from 'fs';
+import { PoplarException } from '../Models/PoplarException';
+import * as log from './Log';
+
+export interface IConfig {
+    /** If it is true, Jobnets are scheduled automatically. */
+    isAutoSchedule: boolean;
+}
+
+/**
+ * Loading config
+ * @throws PoplarException
+ */
+const load = <T>(filepath: string): T => {
+    log.info('%s Loading...', filepath);
+    try {
+        const stringData = readFileSync(filepath, 'utf-8');
+
+        return JSON.parse(stringData) as T;
+    } catch (error) {
+        const e = error as Error;
+        throw new PoplarException(e.message, e.stack);
+    }
+};
+
+/**
+ * Loading config
+ * @throws PoplarException
+ */
+export const loadConfig = (): IConfig => {
+    const path = process.env.SERVER_CONFIG_PATH === undefined ? './config/server.json' : process.env.SERVER_CONFIG_PATH;
+
+    return load(path);
+};
