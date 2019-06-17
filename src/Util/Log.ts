05e6f95e388e65e7f928b16e2d9e3a806a2e6759
nrck <challengerk.8000@gmail.com>
2019-06-17 17:47:48 +0900

    change: output message and stack.
    - if args is error object, message and stack are outputted automatically

--- src/Util/Log.ts
+++ src/Util/Log.ts
@@ -1,5 +1,6 @@
 import * as Moment from 'moment';
 import * as util from 'util';
+import { PoplarException } from '../Models/PoplarException';
 
 export enum LogLevel {
     TRACE = -1,
@@ -11,11 +12,18 @@ export enum LogLevel {
 
 export let logLevel: LogLevel = LogLevel.TRACE;
 
+// colorモジュールに置き換えるかも
+// TODO: replace color module
 const red = '\u001b[31m';
 const yellow = '\u001b[33m';
 const reset = '\u001b[0m';
 
-function outputLog(level: LogLevel, log: string): void {
+/**
+ * outputlog
+ * @param level Output message log level.
+ * @param log Output message
+ */
+const outputLog = (level: LogLevel, log: string): void => {
     if (level < logLevel) {
         return;
     }
@@ -37,21 +45,32 @@ function outputLog(level: LogLevel, log: string): void {
         default:
             break;
     }
-}
+};
 
-function formatLogString(level: LogLevel, args: any[]): string {
+/**
+ * Output log message formatter
+ * @param level Output message log level
+ * @param args util.format() arguments
+ */
+// tslint:disable-next-line: no-any
+const formatLogString = (level: LogLevel, args: any[]): string => {
     const nowStr = Moment().format();
-    const levelStr = `${LogLevel[level]}     `;
+    // tslint:disable-next-line: no-magic-numbers
+    const levelStr = (`${LogLevel[level]}     `).slice(0, 5);
     // tslint:disable-next-line: no-any
     const logStr = util.format.apply<undefined, any[], string>(undefined, args);
     const e = new Error();
-    const frame = e.stack!.split('\n')[3];
+    // tslint:disable-next-line: no-magic-numbers
+    const frames = (e.stack as string).split('\n');
+    const frame = frames[3].split(' ')[5] === 'new' ? frames[4] : frames[3];
     const at = frame.split(':');
+    // tslint:disable-next-line: no-magic-numbers
     const lineNumber = at[at.length - 2];
+    // tslint:disable-next-line: no-magic-numbers
     const functionName = frame.split(' ')[5];
 
-    return util.format('%s [%s] %s (%s:%s)', nowStr, levelStr.slice(0, 5), logStr, functionName, lineNumber);
-}
+    return util.format('%s [%s] %s (%s:%s)', nowStr, levelStr, logStr, functionName, lineNumber);
+};
 
 export function trace(...msg: any[]): void {
     outputLog(LogLevel.TRACE, formatLogString(LogLevel.TRACE, msg));
@@ -70,5 +89,12 @@ export function warn(...msg: any[]): void {
 }
 
 export function error(...msg: any[]): void {
-    outputLog(LogLevel.ERROR, formatLogString(LogLevel.ERROR, msg));
+    if (util.types.isNativeError(msg[0]) || msg[0] instanceof PoplarException) {
+        const message = (msg[0] as Error).message;
+        const stack = (msg[0] as Error).stack === undefined ? 'Stack is undefined' : (msg[0] as Error).stack;
+        outputLog(LogLevel.ERROR, formatLogString(LogLevel.ERROR, [message]));
+        debug(stack);
+    } else {
+        outputLog(LogLevel.ERROR, formatLogString(LogLevel.ERROR, msg));
+    }
 }
