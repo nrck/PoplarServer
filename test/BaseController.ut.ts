b9371c3f4b085d6f7ec5ba45ff82609f923a9fed
nrck <challengerk.8000@gmail.com>
2019-06-23 23:16:11 +0900

    test: BaseController.ut.ts

--- test/BaseController.ut.ts
+++ test/BaseController.ut.ts
@@ -2,32 +2,28 @@ import { BaseController } from '../src/Models/BaseController';
 import { MasterJobnet } from '../src/Models/MasterJobnet';
 import { info } from '../src/Util/Log';
 import { RunJobnet } from '../src/Models/RunJobnet';
+import { JobnetNode } from '../src/Models/JobnetNode';
 
 const newobj = new MasterJobnet();
 newobj.enable = true;
 newobj.info = 'テストジョブネット';
-newobj.name = 'テスト1413';
+newobj.name = 'テスト1750(Node有り)';
 newobj.schedule = {
     'day': {
-        'operation': 'EveryDay'
+        'operation': 'Holiday'
     },
     'deadline': undefined,
     'delay': undefined,
     'month': {
         'operation': 'EveryMonth'
     },
-    'start': '14:13'
+    'start': '17:50'
 };
 
 const Test = async () => {
-    const a = await BaseController.get(RunJobnet, 1);
-    const e = a.entity as RunJobnet;
-    e.state = 'WaitingStartTime';
+    const a = await BaseController.add(MasterJobnet, newobj);
 
-    info('add is ok');
-    const b = await BaseController.update(MasterJobnet, e, { 'id': e.id });
-
-    return b;
+    return a;
 }
 
 Test().then((res) => info(res.message)).catch();
