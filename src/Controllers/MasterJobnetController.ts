fec4a2c615c0f13c0a44e5ee7db8e330174f54c1
nrck <challengerk.8000@gmail.com>
2019-06-24 13:51:40 +0900

    add: MasterJobnetController.ts

---
+++ src/Controllers/MasterJobnetController.ts
@@ -0,0 +1,38 @@
+import { ObjectType } from 'typeorm';
+import { BaseController, FuncReject, FuncResolve, IResponse } from '../Models/BaseController';
+import { DataStore } from '../Models/DataStore';
+import { MasterJobnet } from '../Models/MasterJobnet';
+import { NOT_FOUND, SERVER_ERROR } from '../Models/Types/HttpStateCode';
+import * as log from '../Util/Log';
+
+/**
+ * Master Job Controller
+ */
+export class MasterJobnetController extends BaseController {
+    public static async get(entityClass: ObjectType<MasterJobnet>, id: number): Promise<IResponse<MasterJobnet>>;
+    public static async get(id: number): Promise<IResponse<MasterJobnet>>;
+
+    public static async get(entityClassOrId: ObjectType<MasterJobnet> | number, id?: number): Promise<IResponse<MasterJobnet>> {
+        if (typeof entityClassOrId !== 'number') {
+            return super.get<MasterJobnet>(MasterJobnet, id as number);
+        }
+
+        // tslint:disable-next-line: space-before-function-paren
+        return new Promise(async (resolve: FuncResolve<MasterJobnet>, reject: FuncReject<MasterJobnet>): Promise<void> => {
+            try {
+                const conn = await DataStore.createConnection();
+                const repository = conn.getRepository(MasterJobnet);
+                const object = await repository.findOne(id, { 'relations': ['nodes'] });
+
+                if (object === undefined) {
+                    reject(super.getResponse<MasterJobnet>(object, 'MasterJobnet is not found.', NOT_FOUND));
+                } else {
+                    resolve(BaseController.getResponse<MasterJobnet>(object));
+                }
+            } catch (err) {
+                log.error(err);
+                reject(BaseController.getResponse<MasterJobnet>(undefined, (err as Error).message, SERVER_ERROR));
+            }
+        });
+    }
+}
