3a636f97abee2505d1193aadf7cf791ab4b145c2
nrck <challengerk.8000@gmail.com>
2019-07-04 17:57:46 +0900

    change: Logger header text

--- src/Util/SQLLogger.ts
+++ src/Util/SQLLogger.ts
@@ -3,31 +3,31 @@ import * as log from './Log';
 
 export class SQLLoger implements Logger {
 
-    logQuery(query: string, parameters?: any[] | undefined, queryRunner?: QueryRunner | undefined): void {
-        log.trace('Query:  %s', query);
-        if (parameters !== undefined) log.trace('Parm:   %s', JSON.stringify(parameters));
+    logQuery(query: string, parameters?: any[] | undefined, _queryRunner?: QueryRunner | undefined): void {
+        log.trace('[SQL LOGGER]Query:  %s', query);
+        if (parameters !== undefined) log.trace('[SQL LOGGER]Parm:   %s', JSON.stringify(parameters));
     }
 
     logQueryError(error: string, query: string, parameters?: any[] | undefined, queryRunner?: QueryRunner | undefined) {
-        log.error(error);
-        log.error('Query:  %s', query);
-        if (parameters !== undefined) log.error('Parm:   %s', JSON.stringify(parameters));
-        if (queryRunner !== undefined) log.error('Runner: %s', queryRunner);
+        log.error('[SQL LOGGER]%s', error);
+        log.error('[SQL LOGGER]Query:  %s', query);
+        if (parameters !== undefined) log.error('[SQL LOGGER]Parm:   %s', JSON.stringify(parameters));
+        if (queryRunner !== undefined) log.error('[SQL LOGGER]Runner: %s', queryRunner);
     }
 
     logQuerySlow(time: number, query: string, parameters?: any[] | undefined, queryRunner?: QueryRunner | undefined) {
-        log.warn('This query is slow. time: %d', time);
-        log.warn('Query:  %s', query);
-        if (parameters !== undefined) log.warn('Parm:   %s', JSON.stringify(parameters));
-        if (queryRunner !== undefined) log.warn('Runner: %s', queryRunner);
+        log.warn('[SQL LOGGER]This query is slow. time: %d', time);
+        log.warn('[SQL LOGGER]Query:  %s', query);
+        if (parameters !== undefined) log.warn('[SQL LOGGER]Parm:   %s', JSON.stringify(parameters));
+        if (queryRunner !== undefined) log.warn('[SQL LOGGER]Runner: %s', queryRunner);
     }
 
-    logSchemaBuild(message: string, queryRunner?: QueryRunner | undefined) {
-        log.info('Schema was builded: %s', message);
+    logSchemaBuild(message: string, _queryRunner?: QueryRunner | undefined) {
+        log.info('[SQL LOGGER]Schema was builded: %s', message);
     }
 
-    logMigration(message: string, queryRunner?: QueryRunner | undefined) {
-        log.info('Migrationning: %s', message);
+    logMigration(message: string, _queryRunner?: QueryRunner | undefined) {
+        log.info('[SQL LOGGER]Migrationning: %s', message);
     }
 
     log(level: "warn" | "info" | "log", message: any, queryRunner?: QueryRunner | undefined) {
@@ -35,11 +35,11 @@ export class SQLLoger implements Logger {
             default:
             case 'log':
             case 'info':
-                log.info(message);
+                log.info('[SQL LOGGER]%s', message);
                 break;
             case 'warn':
-                log.warn(message);
-                if (queryRunner !== undefined) log.warn('Runner: %s', queryRunner);
+                log.warn('[SQL LOGGER]%s', message);
+                if (queryRunner !== undefined) log.warn('[SQL LOGGER]Runner: %s', queryRunner);
                 break;
         }
     }
